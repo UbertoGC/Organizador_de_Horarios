a10acb9fcc8f762841c8da0d81a359af4b3502a3
UbertoGC <ugarcia@unsa.edu.pe>
2023-12-12 12:29:54 -0500

    Configurando conexión de BD con el servidor.

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, session, request, redirect, url_for,flash
+import mariadb
+from controladores import ControladorLogin
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = ''
-app.config['MYSQL_USER'] = ''
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = ''
+conn = mariadb.connect(
+         host='127.0.0.1',
+         port=3306,
+         user='root',
+         password='',
+         database='organizadorhorarios')
 
-app.secret_key='mysecretkey'
+cur = conn.cursor()
 
 @app.route('/')
 def home():
+    ControladorLogin.comprobar_login(cur,'ugafuwh@gmail.com','12345678')
     return render_template('index.html')
 
 if __name__=='__main__':
