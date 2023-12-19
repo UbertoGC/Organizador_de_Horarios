db5b5e409037fa95cb147a8b07b394cb85965306
Diego-Aquino1 <daquino@unsa.edu.pe>
2023-12-19 12:19:05 -0500

    Organize files, change to mysqlconnector and create login and register models

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 
 cors = CORS(app)
 
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SECRET_KEY'] = 'h2g3h32gh2'
+
 Session(app)
 #################### ERRORES ########################
 
@@ -49,55 +53,14 @@ def authenticate_user(username, password):
 
 ###################### RUTAS ########################
 
-@app.route('/')
-def login():
-    return render_template('login.html')
-
-@app.route('/month', methods=['GET'])
-def evaluateMonth():
-    selectedMonth = request.args.get('month-input')
-    print(selectedMonth)
-    data_ = model.get_evaluate_month(selectedMonth)
-    return render_template('/index.html', data=data_, selectedMonth_=selectedMonth)
-
-@app.route('/day', methods=['GET'])
-def evaluateDay():
-    selectedDay = request.args.get('day-input')
-    print(selectedDay)
-    data_ = model.get_evaluate_day(selectedDay)
-    return render_template('/index.html', data=data_, selectedDay_=selectedDay)
-
-@app.route('/week', methods=['GET'])
-def evaluateWeek():
-    selectedWeek = request.args.get('selected-week')
-    print(selectedWeek)
-
-@app.route('/evaluate', methods=['GET'])
-def evaluate():
-    selectedMonth = request.args.get('selected-month')
-    selectedDay = request.args.get('selected-day')
-
-    if selectedMonth:
-        data = model.get_evaluate_month(selectedMonth)
-        evaluation_counts = model.get_evaluate_month_count(selectedMonth)
-
-    elif selectedDay:
-        data = model.get_evaluate_day(selectedDay)
-        evaluation_counts = model.get_evaluate_day_count(selectedDay)
-    else:
-        data = []
-
-    return render_template('/index.html', data=data, _evaluation_counts=evaluation_counts)
 
 @app.route('/index')
-def evaluatee():
-    data_ = model.get_evaluate()
-    evaluation_counts = model.get_evaluate_count()
-    return render_template('/index.html', data=data_, _evaluation_counts=evaluation_counts)
+def index():
+    return render_template('/index.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def loginn():
+@app.route('/', methods=['GET', 'POST'])
+def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -106,8 +69,7 @@ def loginn():
 
         if user:
             # Establecer los datos de sesión para el usuario
-            session['username'] = user['username']
-            session['iduser'] = user['iduser']
+            session['email'] = user['email']
 
             return redirect('/index')
         else:
@@ -120,24 +82,24 @@ def loginn():
 def register():
     if request.method == 'POST':
 
-        username = request.form['firstname']
-        password = request.form['lastname']
-        username = request.form['username']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
         password = request.form['password']
 
         data = {
-            'username': username,
+            'firstname': firstname,
+            'lastname': lastname,
+            'email': email,
             'password': password
         }
-
-        headers = {'Content-Type': 'application/json'}
-        response = requests.post(
-            host + '/user/add_user', json=data, headers=headers)
-
-        return redirect('/index')
+        create = model.add_user(data)
+        if(create):
+            return redirect('/login')
+        else:
+            flash('Error', 'error')
 
     return render_template('register.html')
-
 
 @app.route('/logout')
 def logout():
@@ -146,15 +108,8 @@ def logout():
     session.pop('role', None)
 
     # Redirigir al usuario a la página de inicio
-    return render_template('home/index.html')
+    return render_template('index.html')
 
-def execute_arduino_script():
-    arduino_script_path = 'ArduinoPY.py'
-    subprocess.Popen(['python', arduino_script_path])
-def start_arduino_script():
-    p = multiprocessing.Process(target=execute_arduino_script)
-    p.start()
 
 if __name__ == "__main__":      
-    start_arduino_script()
     app.run(debug=True)
--- models/model.py
+++ models/model.py
@@ -4,19 +4,33 @@ class TaskModel:
     def __init__(self):        
         self.mysql_pool = MySQLPool()
 
-    def get_user_username(self):
-        rv = self.mysql_pool.execute('SELECT * from user where email = %(username)s')
-
-    def get_evaluate(self):
-        rv = self.mysql_pool.execute('SELECT * from evaluate')
-        data = []
+    def get_user_username(self, email):
+        params = {'email': email}
+        rv = self.mysql_pool.execute(
+            "SELECT * from user where email=%(email)s", params)
         content = {}
         for result in rv:
-            content = {'id': result[0], 'value': result[1], 'fecha': result[2], 'hora': result[3]}
-            data.append(content)
-            content = {}
-        return data
+            content = {
+                'email': result[0], 'password': result[1]}
+            return content
+        return None
     
+    def add_user(self, data: dict):
+        params = {
+            'email' : data["email"],
+            'password' : data["password"],
+            'firtsName' : data["firstname"],
+            'lastName' : data["lastname"],
+        }
+
+        query = """insert into user (email, password, firstName, lastName)
+            values (%(email)s, %(password)s, %(firtsName)s, %(lastName)s)"""    
+
+        cursor = self.mysql_pool.execute(query, params, commit=True)
+
+        return cursor
+    
+# Example model
     def get_evaluate_count(self):
         sql = """
         SELECT 
@@ -44,140 +58,7 @@ class TaskModel:
             'bad_count': 0
         }
 
-    def get_evaluate_month(self, selectedMonth):
-        params = {'selectedMonth' : selectedMonth}      
-        rv = self.mysql_pool.execute("SELECT * FROM evaluate WHERE DATE_FORMAT(fecha, '%Y-%m') = %(selectedMonth)s", params)
-        data = []
-        content = {}
-        for result in rv:
-            content = {'id': result[0], 'value': result[1], 'fecha': result[2], 'hora': result[3]}
-            data.append(content)
-            content = {}
-        return data
-    
-    def get_evaluate_month_count(self, selectedMonth):   
-        params = {'selectedMonth': selectedMonth}
-        sql = """
-        SELECT 
-            COUNT(*) AS total_registros,
-            SUM(CASE WHEN value = 'good' THEN 1 ELSE 0 END) AS good_count,
-            SUM(CASE WHEN value = 'neutral' THEN 1 ELSE 0 END) AS neutral_count,
-            SUM(CASE WHEN value = 'bad' THEN 1 ELSE 0 END) AS bad_count 
-        FROM evaluate
-        WHERE DATE_FORMAT(fecha, '%Y-%m') = %(selectedMonth)s
-        """
-        
-        result = self.mysql_pool.execute(sql, params)
-
-        if result:
-            result_dict = {
-                'total_registros': result[0][0],
-                'good_count': result[0][1],
-                'neutral_count': result[0][2],
-                'bad_count': result[0][3]
-            }
-            return result_dict
-        return {
-            'total_registros': 0,
-            'good_count': 0,
-            'neutral_count': 0,
-            'bad_count': 0
-        }
-    
-    def get_evaluate_day(self, selectedDay):
-        params = {'selectedDay' : selectedDay}      
-        rv = self.mysql_pool.execute("SELECT * FROM evaluate WHERE DATE(fecha) = %(selectedDay)s", params)
-        data = []
-        content = {}
-        for result in rv:
-            content = {'id': result[0], 'value': result[1], 'fecha': result[2], 'hora': result[3]}
-            data.append(content)
-            content = {}
-        return data
-
-    
-    def get_evaluate_day_count(self, selectedDay):   
-        params = {'selectedDay': selectedDay}
-        sql = """
-        SELECT 
-            COUNT(*) AS total_registros,
-            SUM(CASE WHEN value = 'good' THEN 1 ELSE 0 END) AS good_count,
-            SUM(CASE WHEN value = 'neutral' THEN 1 ELSE 0 END) AS neutral_count,
-            SUM(CASE WHEN value = 'bad' THEN 1 ELSE 0 END) AS bad_count 
-        FROM evaluate
-        WHERE DATE(fecha) = %(selectedDay)s
-        """
-        
-        result = self.mysql_pool.execute(sql, params)
-
-        if result:
-            result_dict = {
-                'total_registros': result[0][0],
-                'good_count': result[0][1],
-                'neutral_count': result[0][2],
-                'bad_count': result[0][3]
-            }
-            return result_dict
-        return {
-            'total_registros': 0,
-            'good_count': 0,
-            'neutral_count': 0,
-            'bad_count': 0
-        }
-    
-    ################### Actividad ################################
-    # Funcion para obtener una actividad por su ID
-    def get_actividad(self, id_act):    
-        params = {'id_act' : id_act}      
-        rv = self.mysql_pool.execute("SELECT * from actividad where id_act=%(id_act)s", params)                
-        data = []
-        content = {}
-        for result in rv:
-            content = {'id_act': result[0], 'nombre': result[1], 'descripcion': result[2], 'fecha': result[3], 'hora_inicio': result[4], 'hora_fin': result[5], 'estado': result[6], 'enlace_reu': result[7]}
-            data.append(content)
-            content = {}
-        return data
-
-    # Funcion para obtener todas las actividades
-    def get_actividads(self):
-        rv = self.mysql_pool.execute("SELECT * from actividad")  
-        data = []
-        content = {}
-        for result in rv:
-            content = {'id_act': result[0], 'nombre': result[1], 'descripcion': result[2], 'fecha': result[3], 'hora_inicio': result[4], 'hora_fin': result[5], 'estado': result[6], 'enlace_reu': result[7]}
-            data.append(content)
-            content = {}
-        return data
-
-    # Funcion para agregar una actividad
-    def add_actividad(self, nombre, descripcion, fecha, hora_inicio, hora_fin, estado, enlace_reu):
-        params = {
-            'nombre' : nombre,
-            'descripcion' : descripcion,
-            'fecha' : fecha,
-            'hora_inicio' : hora_inicio,
-            'hora_fin' : hora_fin,
-            'estado' : estado,
-            'enlace_reu' : enlace_reu
-        }  
-        query = """insert into actividad (nombre, descripcion, fecha, hora_inicio, hora_fin, estado, enlace_reu)
-            values (%(nombre)s, %(descripcion)s, %(fecha)s, %(hora_inicio)s, %(hora_fin)s, %(estado)s, %(enlace_reu)s)"""    
-        cursor = self.mysql_pool.execute(query, params, commit=True)   
-
-        data = {'id_act': cursor.lastrowid, 'nombre': nombre, 'descripcion': descripcion, 'fecha': fecha, 'hora_inicio': hora_inicio, 'hora_fin': hora_fin, 'estado': estado, 'enlace_reu': enlace_reu}
-        return data
-
-    # Funcion para eliminar una actividad
-    def delete_actividad(self, id_act):
-        params = {'id_act' : id_act}      
-        query = """delete from actividad where id_act = %(id_act)s"""    
-        self.mysql_pool.execute(query, params, commit=True)   
-
-        data = {'result': 1}
-        return data
-
-
-################### Administrador ################################
+########################## Podible Admin CRUD ################################
 
     # Funcion para obtener un administrador por su ID
     def get_administrador(self, id_adm):
@@ -223,8 +104,5 @@ class TaskModel:
         data = {'result': 1}
         return data
 
-
 if __name__ == "__main__":    
     tm = TaskModel()     
-
-    #print(tm.create_actividad('prueba 10', 'desde python'))
